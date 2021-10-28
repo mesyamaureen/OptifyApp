58d7fef5c5cd75b541dc9a284a67c1180869ed70
n-h-o-c-h <hochtritt@strato.de>
2021-10-28 12:27:50 +0200

    bis zu entity Data Model - bei mir nicht installiert? Tabelle Waren erstellt und gefüllt

--- Optify/Optify/Models/Ware.vb
+++ Optify/Optify/Models/Ware.vb
@@ -1,15 +1,44 @@
 ﻿Public Class Ware
 
     'Attribute
-    Dim intWID As Integer
-    Dim strWTyp As String
-    Dim dblWPreis As Double
+    Dim mintWID As Integer
+    Dim mstrWTyp As String
+    Dim mdblWPreis As Double
 
     Public Sub New()
-        intWID = -1
-        strWTyp = ""
-        dblWPreis = 0
+        mintWID = -1
+        mstrWTyp = ""
+        mdblWPreis = 0
 
     End Sub
+
+
+    Public Property ID As Integer
+        Get
+            Return mintWID
+        End Get
+        Set(value As Integer)
+            mintWID = value
+        End Set
+    End Property
+
+    Public Property Typ As String
+        Get
+            Return mstrWTyp
+        End Get
+        Set(value As String)
+            mstrWTyp = value
+        End Set
+    End Property
+
+
+    Public Property Preis As Double
+        Get
+            Return mdblWPreis
+        End Get
+        Set(value As Double)
+            mdblWPreis = value
+        End Set
+    End Property
 
 End Class
