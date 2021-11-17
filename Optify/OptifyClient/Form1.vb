8f0bee70a51293637644c4fe75b04f6ef90f700a
mesyamaureen <mesyamaureen@gmail.com>
2021-11-17 11:29:39 +0100

    FormWarenverwaltung

--- Optify/OptifyClient/Form1.vb
+++ Optify/OptifyClient/Form1.vb
@@ -1,13 +1,21 @@
-﻿Public Class frmAlleBestellungen
+﻿Public Class frmAlleWaren
     Private Sub btnLaden_Click(sender As Object, e As EventArgs) Handles btnLaden.Click
         laden()
     End Sub
 
+    'Private Sub laden()
+    '    Dim bestListe As List(Of BestellungServiceReference.Bestellung)
+    '    Dim svc As BestellungServiceReference.IBestellungverwaltung
+    '    svc = New BestellungServiceReference.BestellungverwaltungClient
+    '    bestListe = svc.gibBestellungen
+    '    Me.DataGridView1.DataSource = bestListe
+    'End Sub
+
     Private Sub laden()
-        Dim bestListe As List(Of BestellungServiceReference.Bestellung)
-        Dim svc As BestellungServiceReference.IBestellungverwaltung
-        svc = New BestellungServiceReference.BestellungverwaltungClient
-        bestListe = svc.gibBestellungen
-        Me.DataGridView1.DataSource = bestListe
+        Dim warenListe As List(Of WarenServiceReference.Ware)
+        Dim svc As WarenServiceReference.IWarenverwaltungService
+        svc = New WarenServiceReference.WarenverwaltungServiceClient
+        warenListe = svc.alleWarenLaden
+        Me.DataGridView1.DataSource = warenListe
     End Sub
 End Class
