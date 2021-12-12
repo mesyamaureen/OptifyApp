92be70833abafc8e2730a32bd4b141caf2226605
mesyamaureen <mesyamaureen@gmail.com>
2021-12-12 01:34:47 +0100

    tblKunde,Mitarbeiter, Lieferant mit bspdaten erstellen, Modulklasse Bestellposition erfüllen

--- Optify/Optify/Models/Bestellposition.vb
+++ Optify/Optify/Models/Bestellposition.vb
@@ -1,3 +1,18 @@
 ﻿Public Class Bestellposition
+    Private mbestPoID As Long
+    Private mbestBestellungID As String
+    Private mwareID As String
+
+    Sub New()
+
+    End Sub
+
+    Sub New(pbestPoID As Long, pbestBestellungID As String, pwareID As String)
+        mbestPoID = pbestPoID
+        mbestBestellungID = pbestBestellungID
+        mwareID = pwareID
+    End Sub
+
+    'Konstruktor Entity
 
 End Class
